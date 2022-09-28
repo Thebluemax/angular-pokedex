1ebbc5778fb9ded2aae30b8c8055dc0bc61e7411
Maximiliano Fernández <thebluemax13@gmail.com>
2022-09-28 21:51:42 +0200

    Image an d loading fixed

--- src/app/components/pokedex/components/pages/location/location.component.ts
+++ src/app/components/pokedex/components/pages/location/location.component.ts
@@ -12,10 +12,28 @@ export class LocationComponent implements OnInit {
 
   loading:boolean;
   title:string;
+  maps = [
+    { name: 'johto', map: 'https://upload.wikimedia.org/wikipedia/commons/5/5d/Map_Pok%C3%A9mon_Red_%26_Blue_FR.png' },
+    { name: 'kalos', map: 'https://cdn.bulbagarden.net/upload/b/b1/Kalos_Pok%C3%A9dex_map.png' },
+    { name: 'hoenn', map: 'https://prowiki.info/images/c/c5/Hoenn.png' },
+    { name: 'sinnoh', map: 'https://prowiki.info/images/a/ac/Sinnoh.png' },
+    { name: 'unova', map: 'https://archives.bulbagarden.net/media/upload/b/b4/Unova.png'},
+    { name: 'kanto', map: 'https://prowiki.info/images/7/78/Kanto.png' },
+    { name: 'alola', map: 'https://cdn.bulbagarden.net/upload/6/6c/Alola.png' }];
+
 
   @Input() location:LocationRegion;
   constructor() { }
     ngOnInit() {
 
+    }
+
+    getMap(region: string) {
+      if(!region)
+        return 'assets/img/loading.gif';
+
+      const mapImg = this.maps.find(r => r.name === region);
+      console.log(mapImg);
+      return mapImg.map;
     }
 }
