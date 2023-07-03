7323b6d0651355fb244c4ae0073da7c15a7a76db
Maximiliano Fernández <thebluemax13@gmail.com>
2023-07-03 19:33:24 +0200

    control footer

--- src/app/components/pokedex/components/control-footer/control-footer.component.spec.ts
+++ src/app/components/pokedex/components/control-footer/control-footer.component.spec.ts
@@ -3,16 +3,23 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ControlFooterComponent } from './control-footer.component';
 import { SecondaryScreenService } from 'src/app/components/pokedex/services/secondary-screen.service';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { provideMockStore } from '@ngrx/store/testing';
+import { getMessage } from 'src/app/components/pokedex/components/control-footer/redux/screen.reducer';
 describe('ControlFooterComponent', () => {
   let component: ControlFooterComponent;
   let fixture: ComponentFixture<ControlFooterComponent>;
-
+  let store: Store<AppState>;
+  // let mockStateSelector: MemoizedSelector<AppState, boolean>;
+   const initialState = {screen:{ message: 'test' },
+                         ui:{ isLoading: false }};
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports:[RouterTestingModule],
       declarations: [ ControlFooterComponent ],
-      providers: [SecondaryScreenService]
+      providers: [provideMockStore(),
+        SecondaryScreenService]
     })
     .compileComponents();
   }));
