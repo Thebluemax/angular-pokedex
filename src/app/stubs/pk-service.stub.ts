ef04616b7967f519a673ca8b947db2ef6197f1f0
Maximiliano <thebluemax13@gmail.com>
2025-06-25 23:10:15 +0200

    re structured css (#135)
    * re structured css
    
    * Add shared module to page module
    
    * repair category list component test

--- src/app/stubs/pk-service.stub.ts
+++ src/app/stubs/pk-service.stub.ts
@@ -1,24 +1,48 @@
+import { PokebaseService } from '@core/services/pokebase.service'
+import { SecondaryScreenService } from '@core/services/secondary-screen.service'
 import { of } from 'rxjs';
 
-export class PokemonServiceStub {
-  getItems() {
+
+// Create mock service classes
+export class MockPokebaseService {
+
+    getItems(apiUrl: string, offset: number, limit: number) {
     return of({
-      data: [
-        { id: 1, first_name: 'George', last_name: 'Bluth', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg' },
-        { id: 2, first_name: 'Janet', last_name: 'Weaver', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg' },
-        { id: 3, first_name: 'Emma', last_name: 'Wong', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg' },
-      ],
+      count: 1126,
+      next: `https://pokeapi.co/api/v2/${apiUrl}?offset=${offset + limit}&limit=${limit}`,
+      previous: offset > 0 ? `https://pokeapi.co/api/v2/${apiUrl}?offset=${Math.max(0, offset - limit)}&limit=${limit}` : null,
+      results: [
+        { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
+        { name: 'ivysaur', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
+        { name: 'venusaur', url: 'https://pokeapi.co/api/v2/pokemon/3/' }
+      ]
     });
   }
 
-  getUserDetails() {
+  getInfoLocation(id: string) {
     return of({
-      data: {
-        id: 1,
-        first_name: 'George',
-        last_name: 'Bluth',
-        avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg',
-      },
+      name: 'test-location',
+      region: { name: 'kanto', url: 'region-url' },
+      areas: [{ name: 'test-area', url: 'area-url' }]
+    });
+  }
+  
+  getLocationArea(url: string) {
+    return of({
+      pokemon_encounters: [
+        { pokemon: { name: 'pikachu', url: 'pokemon-url' } }
+      ]
     });
   }
 }
+
+export class MockSecondaryScreenService {
+  resetScreen() {}
+  setScreen(data: any) {}
+}
+
+// Use in providers like this:
+/*providers: [
+  { provide: PokebaseService, useClass: MockPokebaseService },
+  { provide: SecondaryScreenService, useClass: MockSecondaryScreenService },
+]*/
